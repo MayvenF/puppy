6a37c9e311630e3ee14a08dee04586f546ab72dc
Maeven Fanebust <maeven.fanebust@gmail.com>
2023-11-09 15:04:00 +0200

    added dependency on dog

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/MayvenF/puppy
 
 go 1.21.3
+
+require github.com/MayvenF/dog v0.0.0-20231109130053-bc2cdc4dfa23 // indirect
--- puppy.go
+++ puppy.go
@@ -1,9 +1,17 @@
 package puppy
 
+import (
+	"github.com/MayvenF/dog"
+)
+
 func Bark() string {
 	return "woof!"
 }
 
 func Barks() string {
 	return "woof! Woof! Woof!"
+}
+
+func BigBark() string {
+	return dog.WhenGrownUp(Bark())
 }
